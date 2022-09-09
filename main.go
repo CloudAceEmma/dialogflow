8320fe1ef034ce1d3dede4bc7684b8038afbdacd
emma <du.changkun@cloud-ace.com>
2022-09-09 16:56:36 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+// [START import_libraries]
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	dialogflow "cloud.google.com/go/dialogflow/apiv2"
+	dialogflowpb "google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
+)
+
+// [END import_libraries]
+
+// [START dialogflow_detect_intent_text]
+func DetectIntentText(projectID, sessionID, text, languageCode string) (string, error) {
+	ctx := context.Background()
+
+	sessionClient, err := dialogflow.NewSessionsClient(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer sessionClient.Close()
+
+	if projectID == "" || sessionID == "" {
+		return "", errors.New(fmt.Sprintf("Received empty project (%s) or session (%s)", projectID, sessionID))
+	}
+
+	sessionPath := fmt.Sprintf("projects/%s/agent/sessions/%s", projectID, sessionID)
+	textInput := dialogflowpb.TextInput{Text: text, LanguageCode: languageCode}
+	queryTextInput := dialogflowpb.QueryInput_Text{Text: &textInput}
+	queryInput := dialogflowpb.QueryInput{Input: &queryTextInput}
+	request := dialogflowpb.DetectIntentRequest{Session: sessionPath, QueryInput: &queryInput}
+
+	response, err := sessionClient.DetectIntent(ctx, &request)
+	if err != nil {
+		return "", err
+	}
+
+	queryResult := response.GetQueryResult()
+	fulfillmentText := queryResult.GetFulfillmentText()
+	return fulfillmentText, nil
+}
+
+func main() {
+	var projectID = "ca-emma-334503"
+	var sessionID = "123456789"
+	var text = "I know English"
+	var languageCode = "en-US"
+	fulfillmentText, err := DetectIntentText(projectID, sessionID, text, languageCode)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(fulfillmentText)
+}
